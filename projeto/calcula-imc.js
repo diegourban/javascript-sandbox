e2fcf840d30efc4f7e02efbd619a2981e706b0c3
Diego Leonardo Urban <diego.urban88@gmail.com>
2016-10-07 12:26:09 -0300

    aula 3

--- projeto/calcula-imc.js
+++ projeto/calcula-imc.js
@@ -1,9 +1,20 @@
-var peso = 100;
-var altura = 2.00;
+var peso = document.getElementById("peso-1").textContent; // pega o peso do paciente
+var altura = document.getElementById("altura-1").textContent; // pega a altura do paciente
+var nome = document.getElementById("nome-1").textContent; // pega o nome do paciente
 
-if(altura != 0){
-    var imc = peso / (altura * altura);
-    console.log(imc) // 25
+var paciente = {
+    peso : peso,
+    altura : altura,
+    nome : nome
+}
+
+if(paciente.altura != 0){
+    var imc = paciente.peso / (paciente.altura * paciente.altura);
+
+    var tdDoImc = document.getElementById("imc-1"); // pega o td do imc do paciente 1
+    tdDoImc.textContent = imc; //Altera o conteúdo do td para o valor da variável imc
+
+    console.log(imc);
 }else{
-    console.log("Não posso executar uma divisão por 0!");
+    console("Não posso executar uma divisão por 0!");
 }
