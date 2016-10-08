c437c6359d25d6122671724a3bb2c538710ab14d
Diego Leonardo Urban <diego.urban88@gmail.com>
2016-10-08 14:32:23 -0300

    aula 7

---
+++ projeto/adiciona.js
@@ -0,0 +1,26 @@
+var botao = document.querySelector("#adicionar-paciente");
+
+botao.addEventListener("click", function(event) {
+
+	event.preventDefault(); // impede comportamento padrão
+
+	var campoNome = document.querySelector("#campo-nome");
+	var campoPeso = document.querySelector("#campo-peso");
+	var campoAltura = document.querySelector("#campo-altura");
+
+	var pacienteNovo = "<tr class='paciente'>"+
+						"<td class='info-nome'>" + campoNome.value + "</td>"+
+						"<td class='info-peso'>" + campoPeso.value + "</td>"+
+						"<td class='info-altura'>" + campoAltura.value + "</td>"+
+						"<td class='info-imc'></td>"+
+					"</tr>";
+
+	// var tabela = document.getElementsByTagName("table")[0];
+	// document.querySelectorAll("table"); // retorna array
+	var tabela = document.querySelector("table"); // primeiro elemento
+	tabela.innerHTML = tabela.innerHTML + pacienteNovo;
+
+	campoNome.value = "";
+	campoPeso.value = "";
+	campoAltura.value = "";
+});
